fe1f5be902d2773d0bac9bd9207e615d19cf03ca
Andreas Raddau <and.rad@posteo.de>
2020-08-25 20:53:36 +0200

    Added some translation strings

--- templates/content/index.php
+++ templates/content/index.php
@@ -2,9 +2,15 @@
 	<thead>
 		<tr>
 			<th class='selection'></th>
-			<th class='count' data-sort='count'><span class='sort-indicator hidden icon-triangle-s'></span>Amount</th>
-			<th class='name sort' data-sort='name'>Name<span class='sort-indicator hidden icon-triangle-s'></span></th>
-			<th class='aisle sort' data-sort='aisle'>Aisle<span class='sort-indicator hidden icon-triangle-s'></span></th>
+			<th class='count' data-sort='count'>
+				<span class='sort-indicator hidden icon-triangle-s'></span><?php p($l->t('amount')); ?>
+			</th>
+			<th class='name sort' data-sort='name'>
+				<?php p($l->t('name')); ?><span class='sort-indicator hidden icon-triangle-s'></span>
+			</th>
+			<th class='aisle sort' data-sort='aisle'>
+				<?php p($l->t('aisle')); ?><span class='sort-indicator hidden icon-triangle-s'></span>
+			</th>
 		</tr>
 	</thead>
 	<tbody>

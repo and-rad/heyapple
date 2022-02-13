f576f0048b856c6c76af92548414dd30b61e1bab
Andreas Raddau <and.rad@posteo.de>
2022-02-13 21:48:38 +0100

    Added directory for embeddable web content

---
+++ web/web.go
@@ -0,0 +1,17 @@
+package web
+
+import (
+	"embed"
+	"text/template"
+)
+
+var (
+	//go:embed templates
+	Templates embed.FS
+)
+
+var (
+	App   = template.Must(template.ParseFS(Templates, "templates/app.html"))
+	Home  = template.Must(template.ParseFS(Templates, "templates/home.html"))
+	Login = template.Must(template.ParseFS(Templates, "templates/login.html"))
+)

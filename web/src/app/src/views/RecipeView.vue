b861ba8d48f367ade5254af12cdc49563d099ae1
Andreas Raddau <and.rad@posteo.de>
2022-03-09 14:47:44 +0100

    Blocked out frontend recipe table & details area

--- web/src/app/src/views/RecipeView.vue
+++ web/src/app/src/views/RecipeView.vue
@@ -1,12 +1,23 @@
 <script setup>
 import Main from "../components/Main.vue";
 import NewRecipe from "../components/ClickableInput.vue";
-import { inject } from "vue";
+import FoodList from "../components/FoodList.vue";
+import EditImage from "../components/images/ImageEdit.vue";
+import SaveImage from "../components/images/ImageSave.vue";
+import { ref, inject, onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 const log = inject("log");
 const csrf = inject("csrfToken");
+const recipes = inject("recipes");
+
+const filtered = ref([]);
+const current = ref(null);
+const editMode = ref(false);
+const isSaving = ref(false);
+
+const main = ref(null);
 
 function newRecipe(name) {
 	fetch("/api/v1/recipe", {
@@ -29,10 +40,28 @@ function newRecipe(name) {
 		})
 		.catch((err) => log.err(err));
 }
+
+function saveRecipe() {
+	isSaving.value = true;
+	console.log("TODO save recipe");
+}
+
+function showDetails(id) {
+	current.value = filtered.value.filter((f) => f.id == id)[0];
+	main.value.showDetails();
+}
+
+function onEditMode() {
+	editMode.value ? saveRecipe() : (editMode.value = true);
+}
+
+onMounted(() => {
+	filtered.value = recipes.value;
+})
 </script>
 
 <template>
-	<Main>
+	<Main ref="main" @detailVisibility="editMode = false">
 		<template #filter>
 			<section class="new-item">
 				<h2>{{ $t("aria.headnew") }}</h2>
@@ -42,7 +71,30 @@ function newRecipe(name) {
 				<h2>{{ $t("aria.headsearch") }}</h2>
 			</section>
 		</template>
-		<template #main> Recipes </template>
+
+		<template #main>
+			<FoodList :items="filtered" @selected="showDetails" />
+		</template>
+
+		<template #head-details v-if="current">
+			<h2>{{ current.name }}</h2>
+		</template>
+
+		<template #details v-if="current">
+			<section class="tags">
+				<span class="tag">Tag 1</span>
+				<span class="tag">Tag 2</span>
+				<span class="tag">Tag 3</span>
+				<button
+					class="icon async"
+					:disabled="isSaving"
+					@click="onEditMode"
+				>
+					<EditImage v-if="!editMode" />
+					<SaveImage v-if="editMode" />
+				</button>
+			</section>
+		</template>
 	</Main>
 </template>
 

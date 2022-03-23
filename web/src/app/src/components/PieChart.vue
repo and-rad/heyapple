19c68033b3f51da44b48f637219e8df19cc9b62f
Andreas Raddau <and.rad@posteo.de>
2022-03-23 22:46:50 +0100

    Extended pie chart capabilities

--- web/src/app/src/components/PieChart.vue
+++ web/src/app/src/components/PieChart.vue
@@ -1,26 +1,76 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from "vue";
+
+const radius = 112;
+
+const prop = defineProps(["start", "range", "value", "max", "label", "unit", "frac"]);
+
+const formattedValue = computed(() => +(parseFloat(prop.value) || 0).toFixed(prop.frac || 0));
+const formattedMax = computed(() => +(parseFloat(prop.max) || 0).toFixed(prop.frac || 0));
+
+const transform = computed(() => {
+	let rot = -90 + (parseFloat(prop.start) || 0);
+	return `rotate(${rot} 128 128)`;
+});
+
+const fullArc = computed(() => {
+	let ratio = (parseFloat(prop.range) || 0) / 360;
+	return Math.PI * 2 * radius * ratio + " 710";
+});
+
+const valueArc = computed(() => {
+	let normalized = (parseFloat(prop.value) || 0) / (parseFloat(prop.max) || 1);
+	let range = parseFloat(prop.range) || 0;
+	let ratio = (Math.min(normalized, 1) * range) / 360;
+	return Math.PI * 2 * radius * ratio + " 710";
+});
+
+const overArc = computed(() => {
+	let normalized = (parseFloat(prop.value) || 0) / (parseFloat(prop.max) || 1);
+	let range = parseFloat(prop.range) || 0;
+	let ratio = (Math.max(normalized - 1, 0) * range) / 360;
+	return Math.PI * 2 * radius * ratio + " 710";
+});
+</script>
 
 <template>
 	<figure class="pie-chart">
 		<svg viewBox="0 0 256 256">
-			<circle r="112" cx="128" cy="128" />
-			<circle class="good" r="112" cx="128" cy="128" stroke-dasharray="216 710" transform="rotate(16 128 128)" />
+			<circle class="base" r="112" cx="128" cy="128" :transform="transform" :stroke-dasharray="fullArc" />
+			<circle class="good" r="112" cx="128" cy="128" :stroke-dasharray="valueArc" :transform="transform" />
+			<circle class="bad" r="112" cx="128" cy="128" :stroke-dasharray="overArc" :transform="transform" />
 		</svg>
-		<figcaption>123</figcaption>
+		<div>
+			<slot name="details">
+				<span>{{ formattedValue }} {{ unit }}</span>
+				<hr />
+				<span>{{ formattedMax }} {{ unit }}</span>
+			</slot>
+		</div>
+		<figcaption>{{ label }}</figcaption>
 	</figure>
 </template>
 
 <style>
 .pie-chart {
+	white-space: nowrap;
 	position: relative;
-    width: 4em;
-    height: 4em;
+	width: 4em;
+	height: 4em;
+}
+
+.pie-chart hr {
+	border-bottom: var(--border);
 }
 
 .pie-chart circle {
-	stroke: var(--color-good-light);
 	stroke-width: 12;
 	fill: none !important;
+	transition: 0.5s ease-in-out;
+}
+
+.pie-chart circle.base {
+	stroke: var(--color-good-light);
 }
 
 .pie-chart circle.good {
@@ -29,6 +79,8 @@
 
 .pie-chart circle.bad {
 	stroke: var(--color-bad);
+	stroke-width: 16;
+	transition: 0.5s 0.5s ease-in-out;
 }
 
 .pie-chart circle.soft {
@@ -39,8 +91,17 @@
 	position: absolute;
 	left: 0;
 	right: 0;
+	bottom: 0;
+	text-align: center;
+}
+
+.pie-chart > div {
+	position: absolute;
+	left: 50%;
 	top: 50%;
-	transform: translateY(-50%);
+	transform: translateX(-50%) translateY(-50%);
+	min-width: 50%;
+	max-width: 75%;
 	text-align: center;
 }
 </style>

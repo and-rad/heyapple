c873cc9d246dd84da5674b8bc69d03ae6c51f60a
Andreas Raddau <and.rad@posteo.de>
2022-03-26 09:17:47 +0100

    Created custom checkbox component

---
+++ web/src/app/src/components/Checkbox.vue
@@ -0,0 +1,44 @@
+<script setup>
+const prop = defineProps(["checked"]);
+const emit = defineEmits(["change"]);
+
+function onChange(evt) {
+	emit("change", evt);
+}
+</script>
+
+<template>
+	<label>
+		<input type="checkbox" :checked="checked" @change="onChange" />
+		<div></div>
+	</label>
+</template>
+
+<style>
+label > input[type="checkbox"] {
+	display: none;
+}
+
+label > input[type="checkbox"] + div {
+	width: 1.1em;
+	height: 1.1em;
+	border: var(--border);
+	border-radius: 2px;
+	background-repeat: no-repeat;
+	background-size: 1em;
+	background-position: center;
+	transition: 0.25s;
+}
+
+@media (hover: hover) {
+	label:hover > input[type="checkbox"] + div {
+		border-color: var(--color-primary);
+	}
+}
+
+label > input:checked + div {
+	border-color: var(--color-primary);
+	background-color: var(--color-primary);
+	background-image: var(--icon-check);
+}
+</style>

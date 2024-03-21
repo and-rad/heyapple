55052efc52aa6e21977a24c3fb8d369f485fd7c5
Andreas Raddau <and.rad@posteo.de>
2024-03-21 11:10:24 +0100

    Created query to fetch a user's preferences

---
+++ internal/app/prefs.go
@@ -0,0 +1,160 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2021-2024 The HeyApple Authors.
+//
+// Use of this source code is governed by the GNU Affero General
+// Public License as published by the Free Software Foundation,
+// either version 3 of the License, or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////
+
+package app
+
+// AccountPrefs holds all user settings directly related to
+// the user's account.
+type AccountPrefs struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+// UIPrefs holds all user settings related to the presentation
+// of data in a client application. It contains options for
+// filtering, sorting, and display preferences.
+type UIPrefs struct {
+	NeutralCharts     bool `json:"neutralCharts"`
+	TrackSaltAsSodium bool `json:"trackSaltAsSodium"`
+}
+
+// RDIPrefs holds all user settings related to the
+// recommended daily intake of various nutrients.
+//
+// TODO: primary macros should not be in here because
+// users can select different targets for different days.
+type RDIPrefs struct {
+	KCal     float32 `json:"kcal"`
+	Fat      float32 `json:"fat"`
+	FatSat   float32 `json:"fatsat"`
+	FatO3    float32 `json:"fato3"`
+	FatO6    float32 `json:"fato6"`
+	Carbs    float32 `json:"carb"`
+	Sugar    float32 `json:"sug"`
+	Fructose float32 `json:"fruc"`
+	Glucose  float32 `json:"gluc"`
+	Sucrose  float32 `json:"suc"`
+	Fiber    float32 `json:"fib"`
+	Protein  float32 `json:"prot"`
+	Salt     float32 `json:"salt"`
+
+	Potassium  float32 `json:"pot"`
+	Chlorine   float32 `json:"chl"`
+	Sodium     float32 `json:"sod"`
+	Calcium    float32 `json:"calc"`
+	Phosphorus float32 `json:"phos"`
+	Magnesium  float32 `json:"mag"`
+	Iron       float32 `json:"iron"`
+	Zinc       float32 `json:"zinc"`
+	Manganse   float32 `json:"mang"`
+	Copper     float32 `json:"cop"`
+	Iodine     float32 `json:"iod"`
+	Chromium   float32 `json:"chr"`
+	Molybdenum float32 `json:"mol"`
+	Selenium   float32 `json:"sel"`
+
+	VitA   float32 `json:"vita"`
+	VitB1  float32 `json:"vitb1"`
+	VitB2  float32 `json:"vitb2"`
+	VitB3  float32 `json:"vitb3"`
+	VitB5  float32 `json:"vitb5"`
+	VitB6  float32 `json:"vitb6"`
+	VitB7  float32 `json:"vitb7"`
+	VitB9  float32 `json:"vitb9"`
+	VitB12 float32 `json:"vitb12"`
+	VitC   float32 `json:"vitc"`
+	VitD   float32 `json:"vitd"`
+	VitE   float32 `json:"vite"`
+	VitK   float32 `json:"vitk"`
+}
+
+// Prefs holds all individual preferences types and is the
+// object that gets passed around the application.
+type Prefs struct {
+	Account AccountPrefs `json:"account"`
+	RDI     RDIPrefs     `json:"rdi"`
+	UI      UIPrefs      `json:"ui"`
+}
+
+// This contains an average person's recommended daily intake
+// for all the nutrients tracked by the system. It should be
+// multiplied by values computed from a person's specific
+// body composition and living circumstances in order to
+// represent a correct estimation of their actual needs.
+var baseRDIPrefs = RDIPrefs{
+	KCal:       2000,
+	Fat:        60,
+	Carbs:      270,
+	Protein:    80,
+	Fiber:      32,
+	Salt:       5.8,
+	FatSat:     22,
+	FatO3:      1.6,
+	FatO6:      3.2,
+	VitA:       0.9,
+	VitB1:      1.2,
+	VitB2:      1.3,
+	VitB3:      16,
+	VitB5:      5,
+	VitB6:      1.7,
+	VitB7:      0.03,
+	VitB9:      0.4,
+	VitB12:     0.003,
+	VitC:       90,
+	VitD:       0.015,
+	VitE:       15,
+	VitK:       0.12,
+	Potassium:  3400,
+	Chlorine:   2300,
+	Sodium:     2300,
+	Calcium:    1000,
+	Phosphorus: 700,
+	Magnesium:  400,
+	Iron:       8,
+	Zinc:       11,
+	Manganse:   2.3,
+	Copper:     0.9,
+	Iodine:     0.15,
+	Chromium:   0.035,
+	Molybdenum: 0.045,
+	Selenium:   0.055,
+}
+
+// Preferences is a query to collect all user preferences
+// from the database. If successful, the Prefs field will
+// contains all preferences that were found.
+type Preferences struct {
+	ID    int
+	Prefs Prefs
+}
+
+func (q *Preferences) Fetch(db DB) error {
+	if q.ID == 0 {
+		return ErrNotFound
+	}
+
+	user, err := db.UserByID(q.ID)
+	if err != nil {
+		return err
+	}
+
+	q.Prefs.Account.Email = user.Email
+	q.Prefs.RDI = baseRDIPrefs
+
+	return nil
+}
---
+++ internal/app/prefs_test.go
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2021-2024 The HeyApple Authors.
+//
+// Use of this source code is governed by the GNU Affero General
+// Public License as published by the Free Software Foundation,
+// either version 3 of the License, or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////
+
+package app_test
+
+import (
+	"testing"
+
+	"github.com/and-rad/heyapple/internal/app"
+	"github.com/and-rad/heyapple/internal/mock"
+)
+
+var baseRDI = app.RDIPrefs{
+	KCal:       2000,
+	Fat:        60,
+	Carbs:      270,
+	Protein:    80,
+	Fiber:      32,
+	Salt:       5.8,
+	FatSat:     22,
+	FatO3:      1.6,
+	FatO6:      3.2,
+	VitA:       0.9,
+	VitB1:      1.2,
+	VitB2:      1.3,
+	VitB3:      16,
+	VitB5:      5,
+	VitB6:      1.7,
+	VitB7:      0.03,
+	VitB9:      0.4,
+	VitB12:     0.003,
+	VitC:       90,
+	VitD:       0.015,
+	VitE:       15,
+	VitK:       0.12,
+	Potassium:  3400,
+	Chlorine:   2300,
+	Sodium:     2300,
+	Calcium:    1000,
+	Phosphorus: 700,
+	Magnesium:  400,
+	Iron:       8,
+	Zinc:       11,
+	Manganse:   2.3,
+	Copper:     0.9,
+	Iodine:     0.15,
+	Chromium:   0.035,
+	Molybdenum: 0.045,
+	Selenium:   0.055,
+}
+
+func TestPreferences_Fetch(t *testing.T) {
+	for idx, data := range []struct {
+		db   *mock.DB
+		user int
+
+		prefs app.Prefs
+		err   error
+	}{
+		{ //00// id missing or invalid
+			db:  mock.NewDB(),
+			err: app.ErrNotFound,
+		},
+		{ //01// user does not exist
+			db:   mock.NewDB().WithUser(mock.User1),
+			user: 23,
+			err:  app.ErrNotFound,
+		},
+		{ //02// database error
+			db:   mock.NewDB().WithUser(mock.User1).WithError(mock.ErrDOS),
+			user: mock.User1.ID,
+			err:  mock.ErrDOS,
+		},
+		{ //03// success
+			db:   mock.NewDB().WithUser(mock.User1),
+			user: mock.User1.ID,
+			prefs: app.Prefs{
+				Account: app.AccountPrefs{Email: mock.User1.Email},
+				RDI:     baseRDI,
+			},
+		},
+	} {
+		qry := &app.Preferences{ID: data.user}
+		err := qry.Fetch(data.db)
+
+		if err != data.err {
+			t.Errorf("test case %d: error mismatch \nhave: %v\nwant: %v", idx, err, data.err)
+		}
+
+		if qry.Prefs != data.prefs {
+			t.Errorf("test case %d: preferences mismatch \nhave: %v \nwant: %v", idx, qry.Prefs, data.prefs)
+		}
+	}
+}
